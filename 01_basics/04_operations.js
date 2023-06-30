e5abad1bbc13334b6ee3532efb0d95e43e2d777f
akhilesh-m01 <58871782+akhilesh-m01@users.noreply.github.com>
2023-06-30 22:02:33 +0000

    operations in js

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,41 @@
+let value = 3;
+let negValue = -value;
+
+console.log(negValue);
+// -3
+
+let str1 = "hello"
+let str2 = " akhilesh"
+
+let str3 = str1 + str2
+console.log(str3);
+// hello akhilesh
+
+console.log("1"+2);
+// 12
+
+console.log(1+"2");
+// 12
+
+console.log("1"+2+2);
+// 122
+
+console.log(2+2+"1");
+// 41
+
+console.log(+true);
+// 1
+
+let n1 = n2 = n3 = 4;
+console.log(n1,n2,n3);
+// 4 4 4
+
+let c = 1;
+c++;
+console.log(c);
+// 2
+
+console.log(++c);
+// 3
+console.log(c++);
+// 3
