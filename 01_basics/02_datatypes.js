678584c29cce039c5b856d78914853a76e715139
akhilesh-m01 <58871782+akhilesh-m01@users.noreply.github.com>
2023-06-30 20:25:39 +0000

    learnt about datatypes in js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,42 @@
+"use strict"; //treat all code as newer version
+
+// alert(3+3); as we are using node, we can't alert
+
+let name = "akhilesh";
+let age = 18;
+let isLoggedIn = true;
+
+console.log(typeof(name));
+// string
+
+console.log(typeof(age));
+// number
+
+console.log(typeof isLoggedIn);
+// boolean
+
+let country;
+console.log(typeof(country));
+// undefined
+
+let planet = null;
+console.log(typeof(planet));
+// object
+
+console.log(typeof(undefined));
+// undefined
+
+console.log(typeof null);
+// object
+
+
+// datatypes:
+// 1.Number => 2^53
+// 2.bigint =>very large values
+// 3.boolean => true or false
+// 4.string =>" "
+// 5.undefined =>not initialised
+// 6.null => standalone value
+// 7.symbol => unique
+
+// 8.object
