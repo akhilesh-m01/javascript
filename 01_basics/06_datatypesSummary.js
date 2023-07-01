b25737fc81076da6eb805d9b02142c5e210efe80
akhilesh-m01 <58871782+akhilesh-m01@users.noreply.github.com>
2023-07-01 01:36:14 +0000

    stack and heap memory in js

--- 01_basics/06_datatypesSummary.js
+++ 01_basics/06_datatypesSummary.js
@@ -33,3 +33,41 @@ const myfun = function(){
 }
 myfun()
 
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ 
+// stack and heap memory in javascript
+
+// stack -> primitive -->copy
+// heap -> non-primitive  -->reference(original value)
+
+let val1 = "cricket";
+let val2 = val1; // val2 got a copy of val1
+console.log(val2);
+// cricket
+
+val2 = "football";
+console.log(val2);
+// football
+
+console.log(val1);
+// cricket
+
+
+let myObj1 = {
+    name:"akhilesh",
+    favgame: "football",
+}
+
+console.log(myObj1);
+// { name: 'akhilesh', favgame: 'football' }
+
+let myObj2 = myObj1; //points to myobj1 address
+
+myObj2.favgame = "cricket";
+console.log(myObj1);
+// { name: 'akhilesh', favgame: 'cricket' }
+
+console.log(myObj2);
+// { name: 'akhilesh', favgame: 'cricket' }
+
+
