3daf4ab296a310ab54f5e4cd291b3a47ec1c8af6
akhilesh-m01 <58871782+akhilesh-m01@users.noreply.github.com>
2023-07-03 05:17:51 +0000

    date and time

--- 01_basics/09_datentime.js
+++ 01_basics/09_datentime.js
@@ -12,3 +12,13 @@ console.log(myDate.toJSON());
 console.log(myDate.toLocaleDateString());
 console.log(myDate.toUTCString());
 
+console.log(typeof myDate);
+
+let myDate2 = new Date(2023,6,3);
+console.log(myDate2.toDateString());
+let myDate3 = new Date("2023-07-03");
+console.log(myDate3.toDateString());
+
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp); //gives the milliseconds from 1970
