dbc766b57a804d3d0cd40ecd5aff10cda78fa0a1
akhilesh-m01 <58871782+akhilesh-m01@users.noreply.github.com>
2023-07-03 03:58:29 +0000

    todatexyz()

---
+++ 01_basics/09_datentime.js
@@ -0,0 +1,14 @@
+// Dates
+
+let myDate = new Date()
+console.log(myDate);
+
+console.log(myDate.toString());
+
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toUTCString());
+
